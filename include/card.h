156dcd20447cc11b0aa6a6f5dcd5194b9e306c0a
Gabriel Lody <gabriel.lody@gmail.com>
2020-11-28 07:12:22 +0100

    Added JOKER constant to the card class.

--- include/card.h
+++ include/card.h
@@ -5,16 +5,16 @@
 #ifndef CARD_H
 #define CARD_H
 
-#include <stdexcept>											// For std::out_of_range().
+#include <stdexcept>										// For std::out_of_range().
 
-enum {SPADES, HEARTS, DIAMONDS, CLUBS};							// Suits
-enum {ACE = 1, JACK = 11, QUEEN = 12, KING = 13};				// Figures
+enum {SPADES, HEARTS, DIAMONDS, CLUBS};						// Suits
+enum {JOKER, ACE, JACK = 11, QUEEN = 12, KING = 13};		// Figures
 
 class card
 {
 public:	
-	card(int iNum, int iSt);									// Constructor
-	const int number() const;									// Only getters because you will never need to change a card.
+	card(int iNum, int iSt);								// Constructor
+	const int number() const;								// Only getters because you will never need to change a card.
 	const int suit() const;
 	
 private:
@@ -23,11 +23,11 @@ private:
 
 };
 
-card::card(int iNum, int iSt):									// Constructor definition
-	iNumber((iNum < ACE || iNum > KING) ? throw std::out_of_range("*** Bad card *** - Bad number!") : iNum),
+card::card(int iNum, int iSt):								// Constructor definition
+	iNumber((iNum < JOKER || iNum > KING) ? throw std::out_of_range("*** Bad card *** - Bad number!") : iNum),
 	iSuit((iSt < SPADES || iSt > CLUBS) ? throw std::out_of_range("*** Bad card *** - Bad suit!") : iSt) {}
 
-inline const int card::number() const {return iNumber;}			// Getters definitions
+inline const int card::number() const {return iNumber;}		// Getters definitions
 inline const int card::suit() const {return iSuit;}
 
 #endif
