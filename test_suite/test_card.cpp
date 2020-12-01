aa23be4abd450aeb7107af47c0648044bc794de4
Gabriel Lody <gabriel.lody@gmail.com>
2020-12-01 06:15:44 +0100

    Moved the card output function to a dedicated header file.

--- test_suite/test_card.cpp
+++ test_suite/test_card.cpp
@@ -3,11 +3,9 @@
 // Program to test the card class
 
 #include <iostream>						// std::cout
-#include <string>						// std::string
 
-#include "include/card.h"
-
-std::ostream& operator<<(std::ostream& osOut, card& crdCard);
+#include "../include/card.h"
+#include "include/card_output.h"
 
 int main()
 {
@@ -16,63 +14,5 @@ int main()
 	std::cout << std::endl << crdCard << std::endl << std::endl;
 
 	return 0;
-}
-
-std::ostream& operator<<(std::ostream& osOut, card& crdCard)
-{
-	std::string sFigure = "";			// Output strings.
-	std::string sSuit = "";
-
-	switch(crdCard.figure())
-	{
-		case JOKER:
-			sFigure = "Joker";
-			break;
-
-		case ACE:
-			sFigure = "Ace";
-			break;
-
-		case JACK:
-			sFigure = "Jack";
-			break;
-
-		case QUEEN:
-			sFigure = "Queen";
-			break;
-
-		case KING:
-			sFigure = "King";
-			break;
-
-		default:
-			sFigure = std::to_string(crdCard.figure());		// The figure is a number.
-	}
-
-	switch(crdCard.suit())
-	{
-		case SUITLESS:					// It's a joker.
-			sSuit = "";
-			break;
-
-		case SPADES:
-			sSuit = " of Spades";
-			break;
-
-		case HEARTS:
-			sSuit = " of Hearts";
-			break;
-
-		case DIAMONDS:
-			sSuit = " of Diamonds";
-			break;
-
-		case CLUBS:
-			sSuit = " of Clubs";
-	}
-
-	osOut << sFigure << sSuit;
-
-	return osOut;
 }
 
