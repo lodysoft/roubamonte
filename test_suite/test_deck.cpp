2b40c32b5d0c64179e98a54bdac8cbb04cbfbf01
Gabriel Lody <gabriel.lody@gmail.com>
2020-12-03 12:58:21 +0100

    Moved the output of the deck class to a header file.

--- test_suite/test_deck.cpp
+++ test_suite/test_deck.cpp
@@ -5,18 +5,13 @@
 #include <iostream>
 
 #include "../include/deck.h"
-#include "include/card_output.h"
+#include "include/deck_output.h"
 
 int main()
 {
 	deck dckDeck;
 
-	std::cout << "[" << dckDeck.pop() << "]";			// How ugly! TODO: better this algorithm.
-
-	while(!dckDeck.isEmpty())							// That loop is also a candidate for a dedicated function in a separate header file.
-		std::cout << ", [" << dckDeck.pop() << "]";
-
-	std::cout << std::endl << std::endl;
+	std::cout << dckDeck << std::endl << std::endl;
 
 	return 0;
 }
