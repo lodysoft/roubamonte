eb53b8060a00c1375ab1d53a295d70ad6ebbcbbb
Gabriel Lody <gabriel.lody@gmail.com>
2024-12-21 19:35:09 +0100

    Update player.cpp

--- include/player.cpp
+++ include/player.cpp
@@ -2,16 +2,16 @@
 
 #include "player.h"
 
-player::player(deck& dckDeck): hndHand(dckDeck) {}																						// Constructor.
+player::player(deck& dckDeck): hndHand(dckDeck) {}											// Constructor.
 
-stack& player::getStack() {return stkStack;}																							// Stack property getter (dangerous).
-hand& player::getHand() {return hndHand;}																					// Hand property getter.
+stack& player::getStack() {return stkStack;}												// Stack property getter (dangerous).
+hand& player::getHand() {return hndHand;}												// Hand property getter.
 
-const bool player::isMatch(const card& crdTarget, const card& crdSource) const {return crdTarget.figure() == crdSource.figure();}		// Checks whether two cards form a pair.
+const bool player::isMatch(const card& crdTarget, const card& crdSource) const {return crdTarget.figure() == crdSource.figure();}	// Checks whether two cards form a pair.
 
 void player::steal(player& plyOther)
 {
-	while(!plyOther.getStack().isEmpty()) stkStack.push(plyOther.getStack().pop());														// Moves the target stack to this stack (backwards, but who cares?...).
+	while(!plyOther.getStack().isEmpty()) stkStack.push(plyOther.getStack().pop());							// Moves the target stack to this stack (backwards, but who cares?...).
 	stkStack.push(hndHand.discard());																									// Adds the source card to the top of this stack.
 }
 
