cede7993a5c618dd84532fa950470974fe525e2b
Gabriel Lody <gabriel.lody@gmail.com>
2024-12-21 19:49:44 +0100

    Update test_card.cpp

--- test_suite/test_card.cpp
+++ test_suite/test_card.cpp
@@ -2,16 +2,16 @@
 
 // Program to test the card class
 
-#include <iostream>																// std::cout
+#include <iostream>								// std::cout
 
 #include "../include/card.h"
 #include "include/output.h"
 
 int main()
 {
-	card crdCard(QUEEN, HEARTS);												// Invokes the constructor.
+	card crdCard(QUEEN, HEARTS);						// Invokes the constructor.
 
-	std::cout << std::endl << "\t" << crdCard << std::endl << std::endl;		// Displays it.
+	std::cout << std::endl << "\t" << crdCard << std::endl << std::endl;	// Displays it.
 
 	return 0;
 }
