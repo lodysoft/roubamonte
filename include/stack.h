a42d8374edb043bff2fa3d608347004e2ddba9e8
Gabriel Lody <gabriel.lody@gmail.com>
2024-12-21 19:26:12 +0100

    Update stack.h

--- include/stack.h
+++ include/stack.h
@@ -12,11 +12,11 @@
 class stack
 {
 public:
-	card pop();							// Takes a card from the stack.
-	void push(const card crdCard);		// Puts a card on the top of the stack.
-	const card& top() const;			// Returns the top card so we can steal this stack.
-	const int count() const;			// Returns the size of this stack.
-	const bool isEmpty() const;			// Tests whether the cards are over.
+	card pop();			// Takes a card from the stack.
+	void push(const card crdCard);	// Puts a card on the top of the stack.
+	const card& top() const;	// Returns the top card so we can steal this stack.
+	const int count() const;	// Returns the size of this stack.
+	const bool isEmpty() const;	// Tests whether the cards are over.
 
 protected:
 	std::stack<card> crdStack;
