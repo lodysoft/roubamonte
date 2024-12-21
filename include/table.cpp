a8230965c5c80026023bbdbb0ba4fc9bc9cf5c8c
Gabriel Lody <gabriel.lody@gmail.com>
2024-12-21 19:22:56 +0100

    Update table.cpp

--- include/table.cpp
+++ include/table.cpp
@@ -2,13 +2,13 @@
 
 #include "table.h"
 
-table::table(int iPlayers)																// Default constructor.
+table::table(int iPlayers)								// Default constructor.
 {
-	for(int i = 0; i < TABLE_INITIAL_CARDS; i++) crdCards.push_back(dckPile.pop());		// Moves eight cards from the main stack to the table.
+	for(int i = 0; i < TABLE_INITIAL_CARDS; i++) crdCards.push_back(dckPile.pop());	// Moves eight cards from the main stack to the table.
 
 	for(int i = 0; i < iPlayers; i++) plyPlayers.push_back(player(dckPile));
 }
 
-const std::list<card>& table::getCards() const {return crdCards;}						// Getter for table cards.
-const std::vector<player>& table::getPlayers() const {return plyPlayers;}				// Getter for the players collection.
+const std::list<card>& table::getCards() const {return crdCards;}			// Getter for table cards.
+const std::vector<player>& table::getPlayers() const {return plyPlayers;}		// Getter for the players collection.
 
