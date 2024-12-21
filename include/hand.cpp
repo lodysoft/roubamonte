b8a10a6d228e7710e41adffbd3660e274271ac55
Gabriel Lody <gabriel.lody@gmail.com>
2024-12-21 19:38:10 +0100

    Update hand.cpp

--- include/hand.cpp
+++ include/hand.cpp
@@ -11,17 +11,17 @@
  * Creates new hand with HAND_INITIAL_CARDS cards.
  * Immediately fills hand from deck.
  */
-hand::hand(deck& dckDeck): selection_container(dckDeck, HAND_INITIAL_CARDS) {refill(dckDeck);}														// Default constructor.
+hand::hand(deck& dckDeck): selection_container(dckDeck, HAND_INITIAL_CARDS) {refill(dckDeck);}								// Default constructor.
 
 /**
  * Removes and returns selected card.
  * Shifts remaining cards to maintain consecutive indices.
  */
-card hand::discard()																																// Discards the card pointed to by iSelected.
+card hand::discard()																	// Discards the card pointed to by iSelected.
 {
 	card crdTemp = mapCards.at(iSelected);
 
-	for(std::map<int, card>::iterator it = mapCards.upper_bound(iSelected); it != mapCards.end(); it++) std::prev(it)->second = it->second;			// Shifts remaining cards down.
+	for(std::map<int, card>::iterator it = mapCards.upper_bound(iSelected); it != mapCards.end(); it++) std::prev(it)->second = it->second;		// Shifts remaining cards down.
 
 	mapCards.erase(std::prev(mapCards.end()));
 
