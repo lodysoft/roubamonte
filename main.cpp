d35b26c8586662c3b11f617f5530e78c4aba1bdb
appeye <appeolsson@hotmail.com>
2026-03-30 04:49:46 +0200

    correction: phase 4&5 done

--- main.cpp
+++ main.cpp
@@ -11,7 +11,7 @@ int main()
 {
     sf::RenderWindow window(sf::VideoMode(1024, 768), "Roubamonte");
     window.setFramerateLimit(60);
-
+ 
     sf::Font font;
     if (!font.loadFromFile("assets/fonts/consola.ttf"))
         return -1;
